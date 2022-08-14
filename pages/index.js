43b2382b80bd3a5a1515717ce7813fc661a2b4f9
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2022-08-14 04:43:37 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ const Header = () => (
         overflow: hidden;
         line-height: 1.125;
         min-height: 384px;
-        height: 18vh;
+        height: 12vh;
         max-height: 768px;
       }
       .header * {
